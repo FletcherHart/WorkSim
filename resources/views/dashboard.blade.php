8cbbe94598ce74b9dca5a1470ea7838bbdc08ce3
FletcherHart <fletcherhart@gmail.com>
2021-04-29 12:44:31 -0400

    Format dashboard page with placeholders

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,26 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
-        </h2>
+        </h1>
     </x-slot>
 
-    <div class="py-12">
+    <section class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+            <div class="overflow-hidden grid grid-cols-2">
+                <article class="col-span-1 flex flex-col items-center">
+                    <h2 class="text-lg font-semibold">{Placeholder title}</h2>
+                    <h3>{placeholder}</h3>
+                    <br>
+                    <h3>{placeholder}</h3>
+                </article>
+                <article class="col-span-1 flex flex-col items-center">
+                    <h2 class="text-lg font-semibold">{Placeholder title}</h2>
+                    <h3>{placeholder}</h3>
+                    <br>
+                    <h3>{placeholder}</h3>
+                </article>
             </div>
         </div>
-    </div>
+    </section>
 </x-app-layout>
