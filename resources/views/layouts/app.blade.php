804789ef9bb3374cbd392fd282d74799a9929474
FletcherHart <fletcherhart@gmail.com>
2021-04-29 12:36:21 -0400

    Add sidebar component with basic formatting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,25 +21,26 @@
         <div class="min-h-screen">
             @livewire('navigation-menu')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <div class="grid grid-cols-5">
+            <div class="grid grid-cols-5 min-h-screen">
                 <!-- Sidebar -->
-                <section class="">
-                    <h1>Stats</h1>
-                </section>
+                @livewire('sidebar')
+               
 
                 <!-- Page Content -->
-                <main class="col-span-4">
-                    {{ $slot }}
-                </main>
+                <div class="col-span-4 relative">
+                    <!-- Page Heading -->
+                    @if (isset($header))
+                        <header class="shadow">
+                            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                                {{ $header }}
+                            </div>
+                        </header>
+                    @endif
+                    <main>
+                        {{ $slot }}
+                    </main>
+                    <footer class="w-full flex items-center justify-center absolute bottom-0 mb-5 mt-5">&#169; 2021</footer>
+                </div>
             </div>
         </div>
 
---
+++ resources/views/livewire/sidebar.blade.php
@@ -0,0 +1,26 @@
+<section class="flex flex-col items-center border-r border-black h-full">
+    <h1 class="text-2xl font-bold">Character</h1>
+
+    <h2 class="text-lg font-semibold">Occupation</h2>
+
+    <div class="inline">
+        <h3>Salary: {placeholder}</h3>
+    </div>
+
+    <br>
+
+    <h2 class="text-lg font-semibold">Stats</h2>
+    <div>
+        <h3>Energy: {placeholder}</h3> <!--Replace with bar-->
+        <h3>Money: {placeholder}</h3>
+        <h3>Intelligence: {placeholder}</h3>
+        <h3>Fitness: {placeholder}</h3>
+        <h3>Charisma: {placeholder}</h3>
+    </div>
+
+    <br>
+
+    <a href="#" class="bg-blue-600 p-2 text-white">Inventory</a>
+    <a href="#" class="bg-blue-600 p-2 text-white">Owned Properties</a>
+
+</section>
