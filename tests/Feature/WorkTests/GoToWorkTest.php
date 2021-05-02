73a4ae98d0c4896a8e122060842aec141b36d568
FletcherHart <fletcherhart@gmail.com>
2021-05-02 14:53:38 -0400

    Add basic access tests for work page

--- tests/Feature/WorkTests/GoToWorkTest.php
+++ tests/Feature/WorkTests/GoToWorkTest.php
@@ -2,20 +2,38 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class GoToWorkTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     /**
-     * A basic feature test example.
+     * Assert non-authenticated user cannot view work page
      *
      * @return void
      */
-    public function test_example()
+    public function test_non_auth_user_receives_302_on_work_page()
     {
-        $response = $this->get('/');
+        $response = $this->get('/work');
+
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Assert authenticated user CAN view work page
+     *
+     * @return void
+     */
+    public function test_auth_user_can_view_work_page()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $response = $this->get('/work');
 
         $response->assertStatus(200);
     }
