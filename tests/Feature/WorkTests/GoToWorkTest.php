07cfc1e2bc2767758af8485e11f7a7cf00900720
FletcherHart <fletcherhart@gmail.com>
2021-05-03 13:43:10 -0400

    Create test for viewing list of jobs

--- tests/Feature/WorkTests/GoToWorkTest.php
+++ tests/Feature/WorkTests/GoToWorkTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Company;
+use App\Models\Occupation;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -50,5 +52,34 @@ class GoToWorkTest extends TestCase
         $response = $this->get('/work');
 
         $response->assertSee("You are currently unemployed. Please click the button below to look for jobs.");
+    }
+
+    /**
+     * Assert a user visiting employment page sees list of jobs & their companies.
+     *
+     * @return void
+     */
+    public function test_employment_page_displays_list_of_jobs()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $company = Company::factory()->create();
+
+        $numJobs = 3;
+
+        $occupations = Occupation::factory()->count($numJobs)->create();
+
+        foreach($occupations as $occupation) {
+            $occupation->companies()->attach($company->id);
+        }
+
+        $response = $this->get('/employment');
+
+        foreach($occupations as $occupation) {
+            $response->assertSee($occupation->title);
+            $response->assertSee($occupation->description);
+            $response->assertSee($occupation->salary);
+            $response->assertSee($company->company_name);
+        }
     }
 }
