d4bd389a856872d69daf35f7a8bc78a440aafe79
FletcherHart <fletcherhart@gmail.com>
2021-05-03 13:46:14 -0400

    Create Company & Occupation factories for testing

---
+++ database/factories/OccupationFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Occupation;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OccupationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Occupation::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->jobTitle(),
+            'description' => $this->faker->bs(),
+            'salary' => $this->faker->numberBetween(100, 5000),
+        ];
+    }
+}
