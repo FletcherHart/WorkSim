6518745fa3f13c88a3e880fbbdc988679d6008d8
FletcherHart <fletcherhart@gmail.com>
2021-05-03 13:45:41 -0400

    Modify occupation model with many relation

--- app/Models/Occupation.php
+++ app/Models/Occupation.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Occupation extends Model
 {
     use HasFactory;
+
+    public function companies()
+    {
+        return $this->belongsToMany(Company::class);
+    }
 }
