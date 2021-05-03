9acc72c7443b2244bbfbaebf6cb927b070e58792
FletcherHart <fletcherhart@gmail.com>
2021-05-03 13:47:45 -0400

    Change header of work page

--- resources/views/livewire/work.blade.php
+++ resources/views/livewire/work.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h1 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Work') }}
         </h1>
     </x-slot>
 
