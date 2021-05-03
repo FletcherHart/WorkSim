1b1d4a4a89c446e8e3e91053a64c219eba047ce9
FletcherHart <fletcherhart@gmail.com>
2021-05-03 14:10:16 -0400

    Add preliminary layout to employment page

--- resources/views/livewire/employment.blade.php
+++ resources/views/livewire/employment.blade.php
@@ -1,9 +1,23 @@
-<div>
-    @foreach($occupations as $occupation)
-        {{$occupation->title}}
-        {{$occupation->description}}
-        {{$occupation->salary}}
-        {{$occupation->company_name}}
-    @endforeach
-    
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Work') }}
+        </h1>
+    </x-slot>
+
+    <section class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex flex-col items-center">
+                @foreach($occupations as $occupation)
+                    <div class="flex w-full border-black border-left border-right border-top">
+                            <div class="font-bold">{{$occupation->title}}</div>
+                            <div class="">{{$occupation->description}}</div>
+                            {{$occupation->salary}}
+                            {{$occupation->company_name}}
+                    </div>
+                @endforeach
+                
+            </div>
+        </div>
+    </section>
+</x-app-layout>
