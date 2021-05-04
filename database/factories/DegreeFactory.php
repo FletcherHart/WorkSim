40bfe8f18f851b2997ea10e018ccdf5f10bd69ba
FletcherHart <fletcherhart@gmail.com>
2021-05-04 12:11:59 -0400

    Create factory for Degrees

---
+++ database/factories/DegreeFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Degree;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DegreeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Degree::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->bs(),
+            'experience_req' => $this->faker->numberBetween(10, 500),
+        ];
+    }
+}
