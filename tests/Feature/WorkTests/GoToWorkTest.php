ed006f76d447682cddf672f7c89518bd19548269
FletcherHart <fletcherhart@gmail.com>
2021-05-04 13:09:45 -0400

    Add setup to WorkTests + add test for requirements Add setup that generates database vars & relations for testing Add test that verifies user's can see job requirements

--- tests/Feature/WorkTests/GoToWorkTest.php
+++ tests/Feature/WorkTests/GoToWorkTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\Company;
+use App\Models\Degree;
 use App\Models\Occupation;
+use App\Models\OccupationRequirement;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -13,6 +15,33 @@ class GoToWorkTest extends TestCase
 {
 
     use RefreshDatabase;
+
+    /**
+     * Initialize necessary database data
+     *
+     * @return void 
+     */
+    public function setUp():void {
+        parent::setUp();
+
+        $this->company = Company::factory()->create();
+
+        $numJobs = 3;
+        $numDegrees = 3;
+
+        $this->degrees = Degree::factory()->count($numDegrees)->create();
+
+        $this->occupations = Occupation::factory()->count($numJobs)->create();
+
+        foreach($this->occupations as $occupation) {
+            $occupation->companies()->attach($this->company->id);
+            OccupationRequirement::factory(['occupation_id' => $occupation->id])->create();
+        }
+
+        foreach($this->degrees as $degree) {
+            $degree->occupations()->save($this->occupations[rand(0, $numJobs-1)]);
+        }
+    }
 
     /**
      * Assert non-authenticated user cannot view work page
@@ -63,23 +92,40 @@ class GoToWorkTest extends TestCase
     {
         $this->actingAs($user = User::factory()->create());
 
-        $company = Company::factory()->create();
-
-        $numJobs = 3;
-
-        $occupations = Occupation::factory()->count($numJobs)->create();
-
-        foreach($occupations as $occupation) {
-            $occupation->companies()->attach($company->id);
-        }
-
         $response = $this->get('/employment');
 
-        foreach($occupations as $occupation) {
+        foreach($this->occupations as $occupation) {
             $response->assertSee($occupation->title);
             $response->assertSee($occupation->description);
             $response->assertSee($occupation->salary);
-            $response->assertSee($company->company_name);
+            $response->assertSee($this->company->company_name);
+        }
+    }
+
+    /**
+     * Assert displayed jobs also list stat & education requirements
+     *
+     * @return void
+     */
+    public function test_employment_page_displays_occupation_requirements()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $response = $this->get('/employment');
+
+        $degrees = [];
+        $occupation_reqs = [];
+        foreach($this->occupations as $occupation) {
+            array_push($degrees, Degree::where('id', $occupation->degree_id)->first());
+            array_push($occupation_reqs, OccupationRequirement::where('occupation_id', $occupation->id)->first());
+        }
+
+        foreach($degrees as $degree) {
+            $response->assertSee($degree->title);
+        }
+        foreach($occupation_reqs as $req) {
+            $response->assertSee($req->stat);
+            $response->assertSee($req->stat_req);
         }
     }
 }
