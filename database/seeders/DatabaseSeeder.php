87e3fd2083554e4a20ee85f04df910059edd7817
FletcherHart <fletcherhart@gmail.com>
2021-05-04 14:31:26 -0400

    Create database seeder for local testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $jobs = \App\Models\Occupation::factory(10)->create();
+        $degrees = \App\Models\Degree::factory()->count(5)->create();
+        $companies = \App\Models\Company::factory(5)->create();
+
+        foreach($jobs as $job) {
+            $job->companies()->attach($companies[rand(0, 4)]->id);
+            \App\Models\OccupationRequirement::factory(['occupation_id' => $job->id])->create();
+        }
+
+        foreach($degrees as $degree) {
+            $degree->occupations()->save($jobs[rand(0, 9)]);
+        }
     }
 }
