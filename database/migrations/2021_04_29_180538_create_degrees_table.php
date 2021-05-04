51cb6a849fd665a5c698beee35884283703239f3
FletcherHart <fletcherhart@gmail.com>
2021-05-04 14:26:56 -0400

    Fix mispelling in degrees migration

--- database/migrations/2021_04_29_180538_create_degrees_table.php
+++ database/migrations/2021_04_29_180538_create_degrees_table.php
@@ -17,7 +17,7 @@ class CreateDegreesTable extends Migration
             $table->id();
             $table->string('title');
             $table->tinytext('description');
-            $table->unsignedTinyInteger('experience_req')->detault(100);
+            $table->unsignedTinyInteger('experience_req')->default(100);
             $table->timestamps();
         });
     }
