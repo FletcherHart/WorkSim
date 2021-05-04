d57f40e6c07caef1f6b51125e4a237fdead32aea
FletcherHart <fletcherhart@gmail.com>
2021-05-04 12:10:16 -0400

    Create migration for occupation_requirements table

---
+++ database/migrations/2021_05_04_143543_create_occupation_requirements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOccupationRequirementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('occupation_requirements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('degree_id')->nullable()
+                ->constrained('degrees')
+                ->onDelete('cascade');
+            $table->string('stat')->default('none');
+            $table->unsignedTinyInteger('stat_req')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('occupation_requirements');
+    }
+}
