80aeba11bdf8947d27c6ba27ba9f524c6ec564f2
FletcherHart <fletcherhart@gmail.com>
2021-05-05 19:50:12 -0400

    Fix seeder to prevent duplicate entry error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,20 @@ class DatabaseSeeder extends Seeder
         $jobs = \App\Models\Occupation::factory(10)->create();
         $degrees = \App\Models\Degree::factory()->count(5)->create();
         $companies = \App\Models\Company::factory(5)->create();
+        $stats = ['Intelligence', 'Charisma', 'Fitness'];
 
         foreach($jobs as $job) {
             $job->companies()->attach($companies[rand(0, 4)]->id);
-            \App\Models\OccupationRequirement::factory(['occupation_id' => $job->id])->create();
+            for($i = 0; $i<rand(0, 3); $i++) {
+                //Get if exists, create new otherwise
+                $req = \App\Models\OccupationRequirement::firstOrNew([
+                    'occupation_id' => $job->id, 
+                    'stat' => $stats[array_rand($stats, 1)],
+                ]);
+                //Technically might overwrite stat_req but it doesn't matter
+                $req->stat_req = rand(1, 255);
+                $req->save();
+            }
         }
 
         foreach($degrees as $degree) {
