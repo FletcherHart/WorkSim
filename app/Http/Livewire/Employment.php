2a35e1c32e8c752f4324e570fa1a43806785c675
FletcherHart <fletcherhart@gmail.com>
2021-05-05 19:55:13 -0400

    Separate requirements to prevent near duplicates

--- app/Http/Livewire/Employment.php
+++ app/Http/Livewire/Employment.php
@@ -11,10 +11,14 @@ class Employment extends Component
     {
         $occupations = Occupation::join('company_occupation', 'occupations.id', '=', 'company_occupation.occupation_id')
             ->join('companies', 'companies.id', '=', 'company_occupation.company_id')
-            ->join('occupation_requirements as req', 'occupations.id', '=', 'req.occupation_id')
-            ->join('degrees', 'occupations.degree_id', '=', 'degrees.id')
-            ->select('occupations.title', 'occupations.description', 'occupations.salary', 'companies.company_name', 'req.stat', 'req.stat_req', 'degrees.title as degree')
+            ->leftJoin('degrees', 'occupations.degree_id', '=', 'degrees.id')
+            ->select('occupations.id','occupations.title', 'occupations.description', 'occupations.salary', 'companies.company_name', 'degrees.title as degree')
             ->get();
-        return view('livewire.employment', ['occupations'=>$occupations]);
+
+        //Seperated because occupations were being duplicated due to having multiple requirements.
+        $requirements = Occupation::join('occupation_requirements as req', 'occupations.id', '=', 'req.occupation_id')
+            ->select('occupations.id', 'req.stat', 'req.stat_req')
+            ->get();
+        return view('livewire.employment', ['occupations'=>$occupations, 'requirements'=>$requirements]);
     }
 }
