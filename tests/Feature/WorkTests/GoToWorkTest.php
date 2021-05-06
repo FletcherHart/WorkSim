df96a2330108fe6354c0f6aa5abbe04e4976ee13
FletcherHart <fletcherhart@gmail.com>
2021-05-06 11:51:50 -0400

    Separate tests to appropriate files

--- tests/Feature/WorkTests/GoToWorkTest.php
+++ tests/Feature/WorkTests/GoToWorkTest.php
@@ -17,33 +17,6 @@ class GoToWorkTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * Initialize necessary database data
-     *
-     * @return void 
-     */
-    public function setUp():void {
-        parent::setUp();
-
-        $this->company = Company::factory()->create();
-
-        $numJobs = 3;
-        $numDegrees = 3;
-
-        $this->degrees = Degree::factory()->count($numDegrees)->create();
-
-        $this->occupations = Occupation::factory()->count($numJobs)->create();
-
-        foreach($this->occupations as $occupation) {
-            $occupation->companies()->attach($this->company->id);
-            OccupationRequirement::factory(['occupation_id' => $occupation->id])->create();
-        }
-
-        foreach($this->degrees as $degree) {
-            $degree->occupations()->save($this->occupations[rand(0, $numJobs-1)]);
-        }
-    }
-
-    /**
      * Assert non-authenticated user cannot view work page
      *
      * @return void
@@ -83,49 +56,4 @@ class GoToWorkTest extends TestCase
         $response->assertSee("You are currently unemployed. Please click the button below to look for jobs.");
     }
 
-    /**
-     * Assert a user visiting employment page sees list of jobs & their companies.
-     *
-     * @return void
-     */
-    public function test_employment_page_displays_list_of_jobs()
-    {
-        $this->actingAs($user = User::factory()->create());
-
-        $response = $this->get('/employment');
-
-        foreach($this->occupations as $occupation) {
-            $response->assertSee($occupation->title);
-            $response->assertSee($occupation->description);
-            $response->assertSee($occupation->salary);
-            $response->assertSee($this->company->company_name);
-        }
-    }
-
-    /**
-     * Assert displayed jobs also list stat & education requirements
-     *
-     * @return void
-     */
-    public function test_employment_page_displays_occupation_requirements()
-    {
-        $this->actingAs($user = User::factory()->create());
-
-        $response = $this->get('/employment');
-
-        $degrees = [];
-        $occupation_reqs = [];
-        foreach($this->occupations as $occupation) {
-            array_push($degrees, Degree::where('id', $occupation->degree_id)->first());
-            array_push($occupation_reqs, OccupationRequirement::where('occupation_id', $occupation->id)->first());
-        }
-
-        foreach($degrees as $degree) {
-            $response->assertSee($degree->title);
-        }
-        foreach($occupation_reqs as $req) {
-            $response->assertSee($req->stat);
-            $response->assertSee($req->stat_req);
-        }
-    }
 }
