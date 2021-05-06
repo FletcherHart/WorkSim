931718b0069a087d2107afbb3c26d9c69fa7e9f0
FletcherHart <fletcherhart@gmail.com>
2021-05-06 12:49:10 -0400

    Merge eloquent objects & joins into single query

--- app/Http/Livewire/Employment.php
+++ app/Http/Livewire/Employment.php
@@ -11,14 +11,19 @@ class Employment extends Component
     {
         $occupations = Occupation::join('company_occupation', 'occupations.id', '=', 'company_occupation.occupation_id')
             ->join('companies', 'companies.id', '=', 'company_occupation.company_id')
+            ->join('occupation_requirements as req', 'occupations.id', '=', 'req.occupation_id')
             ->leftJoin('degrees', 'occupations.degree_id', '=', 'degrees.id')
-            ->select('occupations.id','occupations.title', 'occupations.description', 'occupations.salary', 'companies.company_name', 'degrees.title as degree')
+            ->select('occupations.id',
+            'occupations.title', 
+            'occupations.description', 
+            'occupations.salary', 
+            'companies.company_name', 
+            'degrees.title as degree',
+            'req.charisma',
+            'req.intelligence',
+            'req.fitness')
             ->get();
 
-        //Seperated because occupations were being duplicated due to having multiple requirements.
-        $requirements = Occupation::join('occupation_requirements as req', 'occupations.id', '=', 'req.occupation_id')
-            ->select('occupations.id', 'req.stat', 'req.stat_req')
-            ->get();
-        return view('livewire.employment', ['occupations'=>$occupations, 'requirements'=>$requirements]);
+        return view('livewire.employment', ['occupations'=>$occupations]);
     }
 }
