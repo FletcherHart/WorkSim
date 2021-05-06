6d3ed5ecb7d734c750f2dbb0760b20d10ed8484c
FletcherHart <fletcherhart@gmail.com>
2021-05-06 16:04:10 -0400

    Create model with relations to user & occupation

---
+++ app/Models/UserOccupation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserOccupation extends Model
+{
+    use HasFactory;
+
+    protected $table = "user_occupation";
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function occupation() {
+        return $this->hasOne(Occupation::class);
+    }
+}
