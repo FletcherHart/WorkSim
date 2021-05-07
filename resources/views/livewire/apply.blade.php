20e68eb86fbf6ebaa06204b9f41553f7a71436a2
FletcherHart <fletcherhart@gmail.com>
2021-05-07 10:11:13 -0400

    Change job application success text

--- resources/views/livewire/apply.blade.php
+++ resources/views/livewire/apply.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if($result)
-        <h2>Congrats!</h2>
+        <h2>Congrats! You have been accepted for the position of {{$title}}</h2>
     @else
         <h2>Better luck next time.</h2>
     @endif
