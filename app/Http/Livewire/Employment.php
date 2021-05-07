33358aebe86ab3ef7c31c6f2a817dda2f5a37075
FletcherHart <fletcherhart@gmail.com>
2021-05-07 10:39:18 -0400

    Fix eloquent query by matching updated tables

--- app/Http/Livewire/Employment.php
+++ app/Http/Livewire/Employment.php
@@ -9,8 +9,7 @@ class Employment extends Component
 {
     public function render()
     {
-        $occupations = Occupation::join('company_occupation', 'occupations.id', '=', 'company_occupation.occupation_id')
-            ->join('companies', 'companies.id', '=', 'company_occupation.company_id')
+        $occupations = Occupation::join('companies', 'companies.id', '=', 'occupations.company_id')
             ->join('occupation_requirements as req', 'occupations.id', '=', 'req.occupation_id')
             ->leftJoin('degrees', 'occupations.degree_id', '=', 'degrees.id')
             ->select('occupations.id',
