e2a8d3c9e5a577286a86b0ea64104d38985e555c
FletcherHart <fletcherhart@gmail.com>
2021-05-08 14:40:35 -0400

    Add mount method + doWork method to pass tests

--- app/Http/Livewire/Work.php
+++ app/Http/Livewire/Work.php
@@ -11,10 +11,37 @@ use App\Models\Occupation;
 
 class Work extends Component
 {
+
+    public $occupation;
+    public $user;
+
+    public function mount() {
+
+        $this->user = auth()->user();
+
+        $this->occupation = Occupation::join(
+            'user_occupation', 'occupations.id', 
+            '=', 
+            'user_occupation.occupation_id'
+        )
+        ->where('user_occupation.user_id', '=', $this->user->id)
+        ->first();
+    }
+
     public function render()
     {
-        $occupation = Occupation::join('user_occupation', 'occupations.id', '=', 'user_occupation.occupation_id')
-            ->where('user_occupation.user_id', '=', auth()->id())->first();
-        return view('livewire.work', ['occupation' => $occupation]);
+        return view('livewire.work', ['occupation' => $this->occupation])
+            ->layout('layouts.app');
+    }
+
+    /**
+     * Spend a single unit of energy to recieve pay. Company pay should also be calculated and applied.
+     * @return void
+     */
+    public function doWork() {
+        $this->user->money += $this->occupation->salary;
+        $this->user->save();
+
+        $this->emit('addMoney');
     }
 }
