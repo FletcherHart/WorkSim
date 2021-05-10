2c567fc8d0218228cb380b3d89eff1e5b491d7ec
FletcherHart <fletcherhart@gmail.com>
2021-05-10 15:08:24 -0400

    Remov redundant column

--- database/migrations/2021_04_29_181003_create_user_degrees_table.php
+++ database/migrations/2021_04_29_181003_create_user_degrees_table.php
@@ -21,7 +21,6 @@ class CreateUserDegreesTable extends Migration
             $table->foreignId('degree_id')
                 ->constrained('degrees')
                 ->onDelete('cascade');
-            $table->unsignedTinyInteger('current_exp')->default(0);
             $table->timestamps();
         });
     }
