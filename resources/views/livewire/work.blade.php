3321e337299bd67b766dcdc7ed00f5d39f455468
FletcherHart <fletcherhart@gmail.com>
2021-05-10 11:02:49 -0400

    Fix issue with work page display

--- resources/views/livewire/work.blade.php
+++ resources/views/livewire/work.blade.php
@@ -1,22 +1,20 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Work') }}
-        </h1>
-    </x-slot>
+<x-slot name="header">
+    <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Work') }}
+    </h1>
+</x-slot>
 
-    <section class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex flex-col items-center">
-                @if($occupation == null)
-                    <h3 class="text-lg font-bold">Unemployed</h3>
-                    <p>You are currently unemployed. Please click the button below to look for jobs.</p>
-                    <br>
-                    <a href="/employment" class="bg-blue-600 p-2 text-white w-max">Look for a job.</a>
-                @else
-                    
-                @endif
-            </div>
+<section class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex flex-col items-center">
+            @if($occupation == null)
+                <h3 class="text-lg font-bold">Unemployed</h3>
+                <p>You are currently unemployed. Please click the button below to look for jobs.</p>
+                <br>
+                <a href="/employment" class="bg-blue-600 p-2 text-white w-max">Look for a job.</a>
+            @else
+                <button class="bg-blue-600 p-2 text-white w-max" wire:click="doWork">Work</button>
+            @endif
         </div>
-    </section>
-</x-app-layout>
+    </div>
+</section>
